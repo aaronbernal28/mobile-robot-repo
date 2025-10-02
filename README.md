4964d22ce993572adffb015e5a2ab24790794c80
aaronbernal28 <aaronbernal28@gmail.com>
2025-10-02 11:38:40 -0300

    Add taller 5

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mobile-robot-repo
+
+This repository contains resources from the course `Robótica Móvil` at the University of Buenos Aires (UBA), taught in the Computer Science Department, 2st semester of 2025. The problems are solved collaboratively with colleagues.
---
+++ volume/ros_intro/src/keys_to_twist.cpp
@@ -0,0 +1,81 @@
+#include "keys_to_twist.h"
+
+using namespace robmovil;
+
+KeysToTwist::KeysToTwist() : Node("nh")
+{
+  // nos susbribimos a los tópicos que reportan cuando se apreta o suelta una tecla.
+  key_up_sub_ = this->create_subscription<keyboard_msgs::msg::Key>("/keyup", 10, std::bind(&KeysToTwist::on_key_up, this, std::placeholders::_1));
+  key_down_sub_ = this->create_subscription<keyboard_msgs::msg::Key>("/keydown", 10, std::bind(&KeysToTwist::on_key_down, this, std::placeholders::_1));
+
+  // anunciamos que vamos a publicar un mensaje de tipo geometry_msgs::msg::Twist en el tópico /cmd_vel.
+  twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+
+  // ...
+
+  up_ = down_ = left_ = right_ = false;
+}
+
+void KeysToTwist::on_key_up(const keyboard_msgs::msg::Key::SharedPtr key_event)
+{
+  geometry_msgs::msg::Twist twist;
+
+  switch( key_event->code )
+  {
+    case keyboard_msgs::msg::Key::KEY_UP:
+      twist.linear.x = 0.0;
+      break;
+
+    case keyboard_msgs::msg::Key::KEY_DOWN:
+      twist.linear.x = 0.0;
+      break;
+
+    case keyboard_msgs::msg::Key::KEY_LEFT:
+      twist.angular.z = 0.0;
+      break;
+
+    case keyboard_msgs::msg::Key::KEY_RIGHT:
+      twist.angular.z = 0.0;
+      break;
+
+    default:
+      return;
+  }
+  // completar ...
+
+  twist_pub_->publish( twist );
+}
+
+void KeysToTwist::on_key_down(const keyboard_msgs::msg::Key::SharedPtr key_event)
+{
+  geometry_msgs::msg::Twist twist;
+
+  switch( key_event->code )
+  {
+    case keyboard_msgs::msg::Key::KEY_UP:
+      twist.linear.x = 0.1;
+      break;
+
+    case keyboard_msgs::msg::Key::KEY_DOWN:
+      // completar ...
+      twist.linear.x = -0.1;
+      break;
+
+    case keyboard_msgs::msg::Key::KEY_LEFT:
+      // completar ...
+      twist.angular.z = 0.1;
+      break;
+
+    case keyboard_msgs::msg::Key::KEY_RIGHT:
+      // completar ...
+      twist.angular.z = -0.1;
+      break;
+
+    default:
+      return;
+  }
+
+  // completar ...
+  
+  twist_pub_->publish( twist );
+}
